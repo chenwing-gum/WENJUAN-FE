68bba8503313169602bebf6c98b26cb0a720eb7b
刚睡醒的番茄 <1480634990@qq.com>
2023-09-20 23:16:37 +0800

    feat: write List&QuestionCard

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+import List from './pages/List'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ background: 'yellow' }}>问卷 FE</h1>
+      <List></List>
+    </div>
+  )
+}
+
+export default App
